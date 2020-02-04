5e225de934396e95b64a68ff1c80e637e2271dcd
SandraJ526 <sandra.jegina@gmail.com>
2020-02-04 20:52:48 +0200

    Add files via upload

---
+++ FirstAndLastDigitSum/src/learn/programming/Main.java
@@ -0,0 +1,26 @@
+package learn.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(-19));
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+
+        if (number < 0) {
+            return -1;
+        }
+
+        int firstDigit = 0;
+        int lastDigit = number % 10;
+
+        while (number > 0) {
+            firstDigit = number % 10;
+            number /= 10;
+        }
+
+        return firstDigit + lastDigit;
+
+    }
+}
