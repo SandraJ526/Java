929831aec260db7b96cbaaa877fd4bf900308f30
SandraJ526 <sandra.jegina@gmail.com>
2020-03-11 17:52:14 +0200

    Add files via upload

---
+++ LargestPrime/src/learn/programming/Main.java
@@ -0,0 +1,21 @@
+package learn.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+    }
+
+    public static int getLargestPrime(int number) {
+
+        if (number < 2) return -1;
+
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                number /= i;
+                i--;
+            }
+        }
+        return number;
+    }
+}
